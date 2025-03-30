256039651e37c1b7b5111b8bb58f024652b52ae4
Linus Jahn <lnj@kaidan.im>
2025-03-30 19:26:36 +0200

    CallStream: Use smart pointer wrappers for gst C APIs

--- src/client/GstWrapper.h
+++ src/client/GstWrapper.h
@@ -5,12 +5,82 @@
 #ifndef GSTWRAPPER_H
 #define GSTWRAPPER_H
 
+#include <gst/gst.h>
+#include <memory>
+
 #include <QLatin1String>
 
 namespace QXmpp::Private {
 
+template<typename T, void(destruct)(T *)>
+class CustomUniquePtr
+{
+    T *m_ptr = nullptr;
+
+public:
+    CustomUniquePtr(T *ptr = nullptr) : m_ptr(ptr) { }
+    CustomUniquePtr(const CustomUniquePtr &) = delete;
+    ~CustomUniquePtr()
+    {
+        if (m_ptr) {
+            destruct(m_ptr);
+        }
+    }
+    CustomUniquePtr &operator=(const CustomUniquePtr &) = delete;
+    CustomUniquePtr<T, destruct> &operator=(T *ptr)
+    {
+        reset(ptr);
+        return *this;
+    }
+    operator T *() const { return m_ptr; }
+    operator bool() const { return m_ptr != nullptr; }
+    T *operator->() const { return m_ptr; }
+    T *get() const { return m_ptr; }
+    T **reassignRef()
+    {
+        reset();
+        return &m_ptr;
+    }
+    void reset(T *ptr = nullptr)
+    {
+        if (m_ptr) {
+            destruct(m_ptr);
+        }
+        m_ptr = ptr;
+    }
+};
+
+template<typename T>
+struct GstDeleter {
+    void operator()(T *ptr) const
+    {
+        if (ptr) {
+            gst_object_unref(ptr);
+        }
+    }
+};
+
+template<typename T>
+class GstObjectPtr : public std::unique_ptr<T, GstDeleter<T>>
+{
+public:
+    GstObjectPtr(T *ptr = nullptr)
+        : std::unique_ptr<T, GstDeleter<T>>(ptr, GstDeleter<T> {}) { }
+
+    T *get() const { return std::unique_ptr<T, GstDeleter<T>>::get(); }
+    operator T *() const { return get(); }
+    operator gpointer() const { return static_cast<gpointer>(get()); }
+};
+
+using GstElementPtr = GstObjectPtr<GstElement>;
+using GstElementFactoryPtr = GstObjectPtr<GstElementFactory>;
+using GstPadPtr = GstObjectPtr<GstPad>;
+using GstSamplePtr = CustomUniquePtr<GstSample, [](GstSample *p) { gst_sample_unref(p); }>;
+using GstBufferPtr = CustomUniquePtr<GstBuffer, [](GstBuffer *p) { gst_buffer_unref(p); }>;
+using GCharPtr = CustomUniquePtr<gchar, [](gchar *p) { g_free(p); }>;
+
 bool checkGstFeature(QLatin1String feature);
 
-}
+}  // namespace QXmpp::Private
 
 #endif
