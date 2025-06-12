d4542a3f2836cc5d00c41c637401385b5caa6a4a
Linus Jahn <lnj@kaidan.im>
2025-06-13 00:30:03 +0200

    Finish port of SM/SASL2 packets

--- src/server/QXmppIncomingClient.cpp
+++ src/server/QXmppIncomingClient.cpp
@@ -272,7 +272,7 @@ void QXmppIncomingClient::handleStanza(const QDomElement &nodeRecv)
             return;
         }
 
-        if (auto auth = Sasl2::Authenticate::fromDom(nodeRecv)) {
+        if (auto auth = elementFromDom<Sasl2::Authenticate>(nodeRecv)) {
             d->saslVersion = QXmppIncomingClientPrivate::Sasl2;
             d->sasl2AuthRequest = std::move(auth);
             d->saslServer = QXmppSaslServer::create(d->sasl2AuthRequest->mechanism, this);
@@ -298,7 +298,7 @@ void QXmppIncomingClient::handleStanza(const QDomElement &nodeRecv)
                 disconnectFromHost();
                 return;
             }
-        } else if (auto response = Sasl2::Response::fromDom(nodeRecv)) {
+        } else if (auto response = elementFromDom<Sasl2::Response>(nodeRecv)) {
             if (!d->saslServer) {
                 warning(u"SASL response received, but no mechanism selected"_s);
                 sendData(serializeXml(Sasl2::Failure()));
@@ -322,7 +322,7 @@ void QXmppIncomingClient::handleStanza(const QDomElement &nodeRecv)
                 sendData(serializeXml(Sasl2::Failure { Sasl::ErrorCondition::NotAuthorized, {} }));
                 disconnectFromHost();
             }
-        } else if (auto abort = Sasl2::Abort::fromDom(nodeRecv)) {
+        } else if (auto abort = elementFromDom<Sasl2::Abort>(nodeRecv)) {
             d->sasl2AuthRequest.reset();
             sendData(serializeXml(Sasl2::Failure { Sasl::ErrorCondition::Aborted, {} }));
         }
@@ -334,7 +334,7 @@ void QXmppIncomingClient::handleStanza(const QDomElement &nodeRecv)
             return;
         }
 
-        if (auto auth = Sasl::Auth::fromDom(nodeRecv)) {
+        if (auto auth = elementFromDom<Sasl::Auth>(nodeRecv)) {
             d->saslVersion = QXmppIncomingClientPrivate::Sasl;
             d->sasl2AuthRequest.reset();
             d->saslServer = QXmppSaslServer::create(auth->mechanism, this);
@@ -360,7 +360,7 @@ void QXmppIncomingClient::handleStanza(const QDomElement &nodeRecv)
                 disconnectFromHost();
                 return;
             }
-        } else if (auto response = Sasl::Response::fromDom(nodeRecv)) {
+        } else if (auto response = elementFromDom<Sasl::Response>(nodeRecv)) {
             if (!d->saslServer) {
                 warning(u"SASL response received, but no mechanism selected"_s);
                 sendData(serializeXml(Sasl::Failure()));
